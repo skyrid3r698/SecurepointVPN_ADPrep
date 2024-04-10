784171361f34f2cbe15fe3c2f43b9a41538992d7
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2024-04-10 09:25:47 +0200

    better visuals

--- script.ps1
+++ script.ps1
@@ -1,14 +1,19 @@
-$vpnusers = Get-ADGroupMember -Identity "GRP_SecurePoint-VPN"
 $AttributeName = "extensionAttribute10"
+$GroupName = "GRP_SecurePoint-VPN"
+
+$vpnusers = Get-ADGroupMember -Identity $GroupName
+
 foreach ($vpnuser in $vpnusers) {
-if ((Get-ADUser -Identity $vpnuser -Properties *).$AttributeName) {
-    Write-Host $vpnuser "has value. Will be skipped." -ForegroundColor Green
+$CurrentAttributeValue = (Get-ADUser -Identity $vpnuser -Properties *).$AttributeName
+if ($CurrentAttributeValue -ne $null) {
+    Write-Host $vpnuser.name "has value: $CurrentAttributeValue Will be skipped." -ForegroundColor Yellow
     } 
     else {
-    Write-Host $vpnuser "has no value. Will be set"
+    Write-Host "$($vpnuser.name) has no value. Will be set"
     [String]$userkey = ""
     1..16 | % { $userkey += $(Get-Random -InputObject A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,2,3,4,5,6,7) }
     Set-ADUser -Identity $vpnuser -Add @{ extensionAttribute10 = $userkey }
+    Write-Host "$($vpnuser.name) Now has value: $userkey" -ForegroundColor Green
     }
-
+    
 }
