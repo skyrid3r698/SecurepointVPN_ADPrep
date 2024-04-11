ae238249a46639ea12994377aa61efc57260060f
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2024-04-11 10:22:17 +0200

    Add QR-Code Generation

--- script.ps1
+++ script.ps1
@@ -1,19 +1,42 @@
+Param(
+    [switch]$genqr
+)
+
 $AttributeName = "extensionAttribute10"
 $GroupName = "GRP_SecurePoint-VPN"
-
+$issuer = "MCOMP_VPN"
 $vpnusers = Get-ADGroupMember -Identity $GroupName
+
+if ($genqr -eq $true) {
+    if ((get-module qrcodegenerator) -ne $null) {
+        }
+    else {
+        Write-Host "QRCodeGenerator module is not installed! Please follow the install"
+        Install-Module QR-CodeGenerator
+        }
+    }
 
 foreach ($vpnuser in $vpnusers) {
 $CurrentAttributeValue = (Get-ADUser -Identity $vpnuser -Properties *).$AttributeName
 if ($CurrentAttributeValue -ne $null) {
     Write-Host $vpnuser.name "has value: $CurrentAttributeValue Will be skipped." -ForegroundColor Yellow
-    } 
+    if ($genqr -eq $true) {
+        mkdir C:\Users\$env:username\Desktop\QR-Codes\
+        New-QRCodeText -Text "otpauth://totp/$($vpnuser.name)?secret=$CurrentAttributeValue&issuer=$issuer&algorithm=SHA1&digits=6&period=30" -OutPath "C:\Users\$env:username\Desktop\QR-Codes\$($vpnuser.name).png"
+        }
+    }
     else {
     Write-Host "$($vpnuser.name) has no value. Will be set"
     [String]$userkey = ""
     1..16 | % { $userkey += $(Get-Random -InputObject A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,2,3,4,5,6,7) }
     Set-ADUser -Identity $vpnuser -Add @{ extensionAttribute10 = $userkey }
     Write-Host "$($vpnuser.name) Now has value: $userkey" -ForegroundColor Green
+    if ($genqr -eq $true) {
+        mkdir C:\Users\$env:username\Desktop\QR-Codes\
+        New-QRCodeText -Text "otpauth://totp/$($vpnuser.name)?secret=$userkey&issuer=$issuer&algorithm=SHA1&digits=6&period=30" -OutPath "C:\Users\$env:username\Desktop\QR-Codes\$($vpnuser.name).png"
+        }
     }
-    
+if ($genqr -eq $true) {
+    explorer.exe "C:\Users\$env:username\Desktop\QR-Codes\$($vpnuser.name).png"
+    }
 }
